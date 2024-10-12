251c2d0527d0f8dd2a714dba4c95eb7387b99431
chi <cldud97@naver.com>
2024-10-12 15:22:11 +0900

    .

---
+++ programmers/dhfqkfmsrhkfgh_re.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <algorithm>
+#include <queue>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+bool solution(string s)
+{
+    queue<int> q;
+    int t;
+    int cnt = 0;
+    bool answer = true;
+
+    for(int i = 0; i < s.size(); i++){
+        q.push(s[i]);
+    }
+
+    while(!q.empty()){
+        // cout << q.front() << " ";
+        if(q.front() == '('){
+            cnt++;
+        }
+        else{
+            cnt--;
+        }
+        if(cnt < 0){
+            answer = false;
+            break;
+        }
+        //cout << cnt << " ";
+        q.pop();
+    }
+    if(cnt != 0){
+        answer = false;
+    }
+    // cout << "cnt : " << cnt;
+    // cout << endl;
+    return answer;
+}
+
+int main(){
+    cout << solution("(()(");
+}
