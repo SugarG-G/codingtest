4512fa185b0bc3e1eec9f1f585c9127b04f743dd
chi <cldud97@naver.com>
2024-10-14 20:28:40 +0900

    .

--- programmers/bridge_truck_re.cpp
+++ programmers/bridge_truck_re.cpp
@@ -10,11 +10,27 @@ int solution(int bridge_length, int weight, vector<int> truck_weights) {
     int cnt = 0;
     int truck_idx = 0;
     int sum = 0;
+    int tmp = 0;
     queue<int> q;
 
     while(1){
-        q.push(truck_weights[truck_idx]);
-        truck_idx++;
+        if(q.size() < bridge_length){
+            sum += truck_weights[truck_idx];
+            if(sum <= weight){
+                q.push(truck_weights[truck_idx]);
+                cnt++;
+            }
+            else{
+                q.push(0);
+                cnt++;
+            }
+            truck_idx++;
+            
+        }
+        else{
+            q.pop();
+            sum -= truck_weights[truck_idx-2];
+        }
         
     }
 
