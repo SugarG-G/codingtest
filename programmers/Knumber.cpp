32194852ae0a6513f3d456d8552bf75c3cacf36b
chi <cldud97@naver.com>
2024-10-21 13:43:43 +0900

    K번째 코드 추가

---
+++ programmers/Knumber.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <stdio.h>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> solution(vector<int> array, vector<vector<int>> commands){
+    vector<int> answer;
+
+    return answer = array;
+}
+
+int main(){
+    solution({1, 5, 2, 6, 3, 7, 4}, {{2,5,3},{4,4,1},{1,7,3}});
+}
