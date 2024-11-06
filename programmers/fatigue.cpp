18fa65d3a0b4e4cf856718bfa78798951bb1a978
chi <cldud97@naver.com>
2024-11-06 14:08:14 +0900

    .

---
+++ programmers/fatigue.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+int solution(int k, vector<vector<int>> dungeons) {
+    int answer = -1;
+    return answer;
+}
+
+int main(){
+    solution(80,{{80,20},{50,40},{30,10}});
+}
