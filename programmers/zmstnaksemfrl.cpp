aed1c857069cae0427e94f936c4cb97ab0661d52
chi <cldud97@naver.com>
2024-11-20 14:28:03 +0900

    ,

--- programmers/zmstnaksemfrl.cpp
+++ programmers/zmstnaksemfrl.cpp
@@ -2,48 +2,36 @@
 #include <vector>
 #include <iostream>
 #include <algorithm>
+#include <queue>
 
 using namespace std;
 
 string solution(string number, int k) {
     string answer = "";
-    vector<int> temp;
     
+    vector<int> t;
+    string s = "";
 
     for(int i = 0; i < number.size(); i++){
-        temp.push_back(int(number[i]) - '0');
-    }
-    vector<bool> visited (temp.size(), true);
-    for(int i = 0; i < number.size() - k; i++){
-        visited[i] = false;
-    }
-    
-    // for(int i = 0; i < temp.size(); i++){
-    //     cout << temp[i] << " ";
-    // }
-    do{
-        string s = "";
-        for(int i = 0; i < number.size(); i++){
-            if(visited[i]){
-                // cout << number[i] << " ";
-                s += number[i];
-                
+        char temp = number[i];
+        int cursor = 0;
+        for(int j = 0; j <= k; j++){
+            if(temp < number[j]){
+                temp = number[j];
+                cursor = j;
             }
         }
-        temp.push_back(stoi(s));
-        // cout << endl;
+        answer += temp;
+        k -= cursor;
+        number.erase(0, cursor + 1);
     }
-    while(prev_permutation(visited.begin(), visited.end()));
 
-    for(int &a : temp){
-        cout << a << " ";
-    }
 
     return answer;
 }
 
 int main(){
-    solution("1924",2);
+    // cout << solution("1924",2);
     // solution("1231234",3);
-    // solution("4177252841",4);
+    cout << solution("4177252841",4); //size = 6
 }
