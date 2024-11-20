a5d337dab000d50eba9947c8f978c6ccbb1e8617
chi <cldud97@naver.com>
2024-11-20 14:39:05 +0900

    .

--- programmers/zmstnaksemfrl.cpp
+++ programmers/zmstnaksemfrl.cpp
@@ -1,37 +1,26 @@
 #include <string>
 #include <vector>
 #include <iostream>
-#include <algorithm>
-#include <queue>
-
 using namespace std;
 
 string solution(string number, int k) {
     string answer = "";
-    
-    vector<int> t;
-    string s = "";
-
-    for(int i = 0; i < number.size(); i++){
-        char temp = number[i];
-        int cursor = 0;
-        for(int j = 0; j <= k; j++){
-            if(temp < number[j]){
-                temp = number[j];
-                cursor = j;
+    answer = number.substr(k); 
+    for(int i = k-1;i >=0;i--){
+        int j = 0;
+        do{
+            if(number[i] >= answer[j]){
+                char temp = answer[j];
+                answer[j] = number[i];
+                number[i] = temp;
+                j++;
+            }else{
+                break;
             }
-        }
-        answer += temp;
-        k -= cursor;
-        number.erase(0, cursor + 1);
+        }while(1);
     }
 
 
-    return answer;
-}
 
-int main(){
-    // cout << solution("1924",2);
-    // solution("1231234",3);
-    cout << solution("4177252841",4); //size = 6
+    return answer;
 }
