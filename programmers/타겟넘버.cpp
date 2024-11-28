397a4b7c31ab5dd25f87dd67fd9f059f280a085a
chi <cldud97@naver.com>
2024-11-28 12:52:49 +0900

    .

--- programmers/타겟넘버.cpp
+++ programmers/타겟넘버.cpp
@@ -6,28 +6,20 @@
 
 using namespace std;
 
-#define max 10
+int answer = 0;
 
-int n,e;
-int graph[max][max];
-bool visited[max];
-
-void dfs(int node){
-    visited[node] = true;
-
-    cout << node << " ";
-
-    for(int next = 0; next < n; ++next){
-        if(!visited[next] && graph[node][next]){
-            dfs(next);
-        }
+void dfs(vector<int> numbers, int target, int sum, int index){
+    if(index == numbers.size()){
+        if(sum == target) answer++;
+        return;
     }
+    dfs(numbers, target, sum + numbers[index], index+1);
+    dfs(numbers, target, sum - numbers[index], index+1);
 }
 
 int solution(vector<int> numbers, int target) {
-    int answer = 0;
 
-
+    dfs(numbers,target,0,0);
 
     return answer;
 }
