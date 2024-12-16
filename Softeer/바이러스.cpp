5950cdd90fcba6df0afc87452cd4a284b84a120b
chi <cldud97@naver.com>
2024-12-16 19:03:02 +0900

    .

--- Softeer/바이러스.cpp
+++ Softeer/바이러스.cpp
@@ -6,10 +6,15 @@ using namespace std;
 
 int main(int argc, char** argv)
 {
-    int k, p, n;
+    unsigned long long k, p, n, mod = 1000000007;
     freopen("input.txt", "r", stdin);
+    cin >> k >> p >> n;
+    // 1000000007
+    for(int i = 0; i < n; i++){
+        k = (k * p) % mod;
+    }
+    cout << k;
 
-    
 
     return 0;
 }
