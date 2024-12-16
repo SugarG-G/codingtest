8b8f4b073bb3f400a0b2227765981f15a5b7070b
chi <cldud97@naver.com>
2024-12-16 16:51:40 +0900

    t1

--- Softeer/8단변속기.cpp
+++ Softeer/8단변속기.cpp
@@ -9,6 +9,9 @@ using namespace std;
 int main(int argc, char** argv)
 {
     vector<int> gear;
+    vector<int> ascending {1,2,3,4,5,6,7,8};
+    vector<int> descending {8,7,6,5,4,3,2,1};
+
     int g;
     freopen("intput.txt", "r", stdin);
     
@@ -17,6 +20,17 @@ int main(int argc, char** argv)
         gear.emplace_back(g);
     }
 
+    for(int i = 0; i < gear.size(); i++){
+        if(ascending[i] == gear[i]){
+            cout << "ascending";
+        }
+        else if(descending[i] == gear[i]){
+            cout << "descending";
+        }
+        else{
+            cout << "mixed";
+        }
+    }
 
     return 0;
 }
