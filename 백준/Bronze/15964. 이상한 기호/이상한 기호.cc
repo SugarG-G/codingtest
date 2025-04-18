5c654ab1331824f6560e9f9237301dad5e5b10d0
SugarG-G <cldud97@naver.com>
2025-04-19 00:40:00 +0900

    [Bronze V] Title: 이상한 기호, Time: 0 ms, Memory: 2020 KB, Score: 30 point -BaekjoonHub

---
+++ 백준/Bronze/15964. 이상한 기호/이상한 기호.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <algorithm>
+#include <string.h>
+#include <vector>
+#include <time.h>
+
+using namespace std;
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+
+    int a, b;
+    
+
+    cin >> a >> b;
+
+    cout << (a+b)*(a-b);
+}
