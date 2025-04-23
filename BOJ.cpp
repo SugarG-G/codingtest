ab969c37902aebe74f02f263cba2d90e86af0988
chi_desktop <cldud97@naver.com>
2025-04-23 21:14:17 +0900

    BOJ폴더삭제 및 algorithm폴더추가

---
+++ BOJ.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <algorithm>
+#include <string.h>
+#include <vector>
+#include <math.h>
+
+using namespace std;
+
+void quicksort(int *arr, int pivot ,int size){
+
+}
+
+int main()
+{
+    freopen("input.txt", "r", stdin);
+
+    int n = 0;
+
+    int array[10000001] = {0, };
+
+    for(int i = 0 ; i < n; i++){
+        scanf("%d", array[i]);
+    }
+
+    
+
+}
