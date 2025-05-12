b1f808c0cc90df4677bf9195a9064612f56ac94b
Yun Chi Yeong <cldud97@naver.com>
2025-05-12 23:06:29 +0900

    [Bronze II] Title: Hashing, Time: 0 ms, Memory: 2024 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.cc
+++ 백준/Bronze/15829. Hashing/Hashing.cc
@@ -4,33 +4,26 @@
 #include <vector>
 
 using namespace std;
-//97 98 99 100 101 102 103 104 105 106 .... 122
-//1 2 3 4 5 6 7 8 9 10
-
-int pow(int a, int b){
-    int result = 1;
-
-    for(int i = 0; i < b; i++){
-        result = result * a;
-    }
-    return result;
-}
 
 int main() {
     // freopen("input.txt", "r", stdin);
 
-    int L, r = 31, M = 1234567891;
-    int hash_value = 0;
-    string str = "";
-    // char *str = (char *)calloc(L, sizeof(char));
-    cin >> L;
-    cin >> str; 
-    
-    for(int i = 0; i < str.size(); i++){
-        hash_value += ((str[i]) - 'a' + 1) * pow(r, i);
-    }
-    cout << hash_value % M;    
+    int L;
+    long long m = 1234567891;
+    long long r = 31, R = 1;
+    long long hash_value = 0;
+    string str;
 
-    // free(str);
+    cin >> L;
+    cin >> str;
+    // cout << (str[3] - 'a' + 1) << '\n';
+    for(int i = 0; i < L; i++){
+        hash_value += ((str[i] - 'a' + 1) * R) % m;
+        hash_value %= m;
+        R = (R * r) % m;
+    }
+    cout << hash_value;
+
+    
     return 0;
 }
