23d2a391cc001943c9e300585425c1c4235fe555
Yun Chi Yeong <cldud97@naver.com>
2025-05-18 20:52:46 +0900

    [Bronze V] Title: A+B - 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2558. A＋B － 2/A＋B － 2.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int A, B;
+    cin >> A >> B;
+    cout << A + B;
+
+
+    return 0;
+}
